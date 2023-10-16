6e6ff3118995db25fa0241a19d765199127cce21
Jan Sobolciak <jan.sobolciak@gmail.com>
2023-10-16 21:07:06 +0200

    Add DB, EF, ApplicationDbContext+DBSet, connectionString

---
+++ TradingApplicationWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TradingApplicationWeb.Models;
+
+namespace TradingApplicationWeb.Data
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        DbSet<FinancialProduct> FinancialProducts { get; set; }
+    }
+
+}
---
+++ TradingApplicationWeb/Models/FinancialProduct.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TradingApplicationWeb.Models
+{
+    public class FinancialProduct
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Symbol { get; set; }
+        [Required]
+        public string From { get; set; }
+        [Required]
+        public double Open { get; set; }
+        [Required]
+        public double High { get; set; }
+        [Required]
+        public double Low { get; set; }
+        [Required]
+        public double Close { get; set; }
+        [Required]
+        public int Volume { get; set; }
+    }
+}
