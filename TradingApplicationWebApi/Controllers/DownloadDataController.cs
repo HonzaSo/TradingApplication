c4aa60b00e64cf406ada90b38bca49c16db3ccb4
Jan Sobolciak <jan.sobolciak@gmail.com>
2023-10-17 11:17:02 +0200

    new projekts - Trad..App..WebApi + new DownloadDataController + new api API/DownloadData

---
+++ TradingApplicationWebApi/Controllers/DownloadDataController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace TradingApplicationWebApi.Controllers
+{
+    [ApiController]
+    public class DownloadDataController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public DownloadDataController( IConfiguration configuration)
+        {
+            //_logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("API/DownloadData")]
+        public string DownloadDataForSymbolByDate(string symbol, string date)
+        {
+            string finalyUrl = GetFinalyUrl(symbol, date);
+            return DownloadData(finalyUrl);
+        }
+
+        private string GetFinalyUrl(string symbol, string date)
+        {
+            string url = $"{_configuration["BaseUrlFY"]}/";
+            return $"{_configuration["BaseUrlFY"]}/" +
+                $"{symbol}/{date}?adjusted=true&apiKey={_configuration["ApiKeyFY"]}";
+        }
+
+        private string DownloadData(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            Task<string> ts = httpClient.GetStringAsync(url);
+            return ts.Result;
+        }
+
+
+    }
+}
