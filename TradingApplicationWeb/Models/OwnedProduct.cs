900cd52683f40c3ccf8824696220e635c09ff847
Jan Sobolciak <jan.sobolciak@gmail.com>
2023-10-19 11:56:20 +0200

    new model Models/OwnedProduct + empty dir Interfaces

---
+++ TradingApplicationWeb/Models/OwnedProduct.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TradingApplicationWeb.Models
+{
+    public class OwnedProduct
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Symbol { get; set; }
+        [Required]
+        public double BuyingDate { get; set; }
+        [Required]
+        public int OwnedProductCount { get; set; }
+        [Required]
+        public double BuyingPrice { get; set; }
+        public FinancialProduct FinancialProductId { get; set; }
+    }
+}
